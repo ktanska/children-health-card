f98583d86e924fa4e8c158a9f7624abf1b458892
kingatan7 <kinga-tanska@wp.pl>
2020-10-17 19:17:28 +0200

    Initial commit
    Signed-off-by: kingatan7 <kinga-tanska@wp.pl>

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
+var hbs = require('handlebars');
+const admin = require('firebase-admin');
+
+
+const app = express();
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+var serviceAccount = require("./children-health-card-firebase-adminsdk-ncwgz-d4f4b52ef4.json");
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+databaseURL: "firebase-adminsdk-ncwgz@children-health-card.iam.gserviceaccount.com"
+});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
